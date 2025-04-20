e31ff9855451d40fd18f185d7ae83942251c22ee
ZacharyLain <zacharytlain@gmail.com>
2025-04-20 05:49:36 -0400

    Teleop using an xbox controller

---
+++ ora_teleop/launch/joystick.launch.py
@@ -0,0 +1,35 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, TimerAction
+from launch_ros.actions import Node
+
+def generate_launch_description():
+  pkg_share = get_package_share_directory('ora_teleop')
+
+  joystick_params = os.path.join(pkg_share, 'config', 'joystick.yaml')
+
+  joystick_node = Node(
+    package='joy',
+    executable='joy_node',
+    parameters=[joystick_params],
+  )
+  
+  teleop_node = Node(
+    package='teleop_twist_joy',
+    executable='teleop_node',
+    name='teleop_node',
+    parameters=[joystick_params],
+    remappings=[
+      ('/cmd_vel', '/diff_drive_cont/cmd_vel_unstamped'),
+    ],
+  )
+
+  return LaunchDescription([
+    joystick_node,
+    teleop_node,
+  ])
+
